31e9905f91d9f0a13442d865cc1c6bc94f8241e2
Jean Hidalgo <jeanhc.dev@gmail.com>
2025-07-27 01:01:16 -0400

    Automatizar la creación del superusuario en el despliegue

--- build.sh
+++ build.sh
@@ -3,4 +3,22 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py migrate
+
+# Crea el superusuario si las variables de entorno existen y el usuario no
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password and not User.objects.filter(username=username).exists():
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superusuario '{username}' creado.")
+else:
+    print(f"Superusuario '{username}' ya existe o no se definieron las variables, omitiendo creación.")
+EOF
